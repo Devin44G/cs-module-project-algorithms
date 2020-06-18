c9032f90d3fce40079f40c296e989c87a6b9cef2
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-06-18 13:10:48 -0500

    eating cookies and window max functions written and passing tests

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,13 +3,16 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
-
-    pass
+    max_val = []
+    for i, num in enumerate(nums):
+        window = nums[i:i + k]
+        if len(window) == k:
+            max_val.append(max(window))
+    return max_val
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
